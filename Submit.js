f41dadbe2d5896e30998cc4792214ddf27a0ec45
Kaintara <barli2k@gmail.com>
2025-09-11 19:42:01 +0100

    Testing API works and learning how to use it.

--- Submit.js
+++ Submit.js
@@ -10,30 +10,30 @@ form.addEventListener('submit', function(event){
     const city = document.getElementById('city')
     const Notes = document.getElementById('add')
     const slider1Value = areas[Number(slider1.value) - 1];
-    const slider2Value = temps[Number(slider1.value) - 1];
-    const slider3Value = dis[Number(slider1.value) - 1];
-    const slider4Value = saf[Number(slider1.value) - 1];
-    const slider5Value = type[Number(slider1.value) - 1];
-    const slider6Value = food[Number(slider1.value) - 1];
-    const slider7Value = days[Number(slider1.value) - 1];
-    const Bud = ''
+    const slider2Value = temps[Number(slider2.value) - 1];
+    const slider3Value = dis[Number(slider3.value) - 1];
+    const slider4Value = saf[Number(slider4.value) - 1];
+    const slider5Value = type[Number(slider5.value) - 1];
+    const slider6Value = food[Number(slider6.value) - 1];
+    const slider7Value = days[Number(slider7.value) - 1];
+    var Bud = ''
     const Buds = document.getElementsByName('Budget') 
     Buds.forEach(radio => {
         if (radio.checked) {
-            Bud = radio.textContent
+            Bud = radio.title
         }
     })
-    const Grp = ''
+    var Grp = ''
     const Grps = document.getElementsByName('Group') 
     Grps.forEach(radio => {
         if (radio.checked) {
-            Grp = radio.textContent
+            Grp = radio.title
         }
     })
     
     const UserInput = {
-        User_Country: country,
-        User_City: city,
+        User_Country: country.textContent,
+        User_City: city.textContent,
         Budget: Bud,
         Area: slider1Value,
         Weather: slider2Value,
@@ -43,7 +43,20 @@ form.addEventListener('submit', function(event){
         Food: slider6Value,
         Group: Grp,
         Days: slider7Value,
-        AddNotes: Notes,
+        AddNotes: Notes.textContent,
     }
+
+  console.log(UserInput.Budget)
+
+    fetch ('https://ai.hackclub.com/chat/completions', {
+        method: 'POST',
+        headers: { "Content-Type": 'application/json'},
+        body: JSON.stringify({"messages":[{"role": "user", "content": "Tell me a joke Return ONLY a JSON object with keys 'joke' and 'category'. Do NOT include any extra text."}]}),
+    }).then(result => result.json())
+    .then(eventsResponse =>{
+        const events = eventsResponse.choices[0].message.content
+
+        console.log(events)
+    })
     
 })
