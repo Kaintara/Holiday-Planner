a10d05936b15e916b7a9f8565f8586d36c875fa5
Kaintara <barli2k@gmail.com>
2025-09-11 15:49:19 +0100

    Slider now Outputs Texts that I require.

--- Planner.js
+++ Planner.js
@@ -1,5 +1,15 @@
+// Code for Sliders
+const slider = document.getElementById('slider');
+const output = document.getElementById('sliderValue');
+const areas = ["Inner City","Green Urban","Balanced","Nature-Focused Towns","Remote Nature"]
 
 
-function generateTopThree() {
+if (slider.value == '3') {
+    output.textContent = 'Inner City';
+} 
 
-}
+slider.addEventListener('input', function() {
+    const index = Number(slider.value) - 1
+    output.textContent = areas[index];
+})
+
